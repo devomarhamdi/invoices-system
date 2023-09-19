920c20b1024419727d46058f5aab593d508e0d27
Omar <devomarhamdi@gmail.com>
2023-09-19 18:47:35 +0300

    added the sections and the invoices

---
+++ app/Http/Controllers/SectionsController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sections;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SectionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('sections.sections');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'section_name' => 'required|unique:sections|max:255',
+        ],[
+
+            'section_name.required' =>'يرجي ادخال اسم القسم',
+            'section_name.unique' =>'اسم القسم مسجل مسبقا',
+
+
+        ]);
+
+        Sections::create([
+            'section_name' => $request->section_name,
+            'description' => $request->description,
+            'created_by' => (Auth::user()->name),
+
+        ]);
+        session()->flash('Add', 'تم اضافة القسم بنجاح ');
+        return redirect('/sections');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sections $sections)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sections $sections)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Sections $sections)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sections $sections)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\InvoicesController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SectionsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,12 +24,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+/*Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+*/
 require __DIR__.'/auth.php';
+
+Route::resource('/invoices', InvoicesController::class);
+Route::resource('/sections', SectionsController::class);
 
 Route::get('/{page}', 'App\Http\Controllers\AdminController@index');
